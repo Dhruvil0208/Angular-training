9fbea520fe5a9c2b0ec52ced23a06c3dd5dc977f
Dhruvil0208 <chaudharydhruvil671@gmail.com>
2023-02-09 14:22:40 +0530

    textbox value and display

--- training/src/app/app.component.ts
+++ training/src/app/app.component.ts
@@ -9,11 +9,12 @@ export class AppComponent {
   title = 'training';
   data = 'code step by step';
   getValue(name:string){
-      alert(name)
+      console.log(name)
+      this.displayValue= name;
   }
-  getdata(val : string){
-    console.warn(val)
-  }
-  
+  // getdata(val : string){
+  //   console.warn(val)
+  // }
+  displayValue:string = '';
 
 }
