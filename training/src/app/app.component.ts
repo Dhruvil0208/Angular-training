577728661e28179f375dccb3f53243ae566d7281
Dhruvil0208 <chaudharydhruvil671@gmail.com>
2023-02-09 14:30:37 +0530

    counter using various thing learnt till now

--- training/src/app/app.component.ts
+++ training/src/app/app.component.ts
@@ -6,15 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'training';
-  data = 'code step by step';
-  getValue(name:string){
-      console.log(name)
-      this.displayValue= name;
-  }
-  // getdata(val : string){
-  //   console.warn(val)
+  // title = 'training';
+  // data = 'code step by step';
+  // getValue(name:string){
+  //     console.log(name)
+  //     this.displayValue= name;
   // }
-  displayValue:string = '';
-
+  // // getdata(val : string){
+  // //   console.warn(val)
+  // // }
+  // displayValue:string = '';
+  count:number = 0;
+  counter( type : string){
+    if(type === 'incr'){
+      this.count++;
+    }
+    else if(type === 'decr'){
+      this.count--;
+    }
+  }
 }
