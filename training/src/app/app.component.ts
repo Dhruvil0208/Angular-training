44bfa1bdd20109b2a997cf1c7a7ac409e7ab62dd
Dhruvil0208 <chaudharydhruvil671@gmail.com>
2023-02-10 14:51:20 +0530

    style binding

--- training/src/app/app.component.ts
+++ training/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { compilePipeFromMetadata } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,50 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   title = 'training';
-  // data = 'code step by step';
-  // getValue(name:string){
-  //     console.log(name)
-  //     this.displayValue= name;
-  // }
-  // // getdata(val : string){
-  // //   console.warn(val)
-  // // }
-  // displayValue:string = '';
-  // count:number = 0;
-  // counter( type : string){
-  //   if(type === 'incr'){
-  //     this.count++;
-  //   }
-  //   else if(type === 'decr'){
-  //     this.count--;
-  //   }
-  //}
-  // name = "string";
-  // disable = false;
-  // show = true;
-  // color = 'green';
-
-  //users = ['dhruvil','ruchit','meet'];
-  users=[
-    {
-      name: 'dhruvil',
-      email:'dhruvil@test.com',
-      phone:'8888', 
-      accounts: ['facebook','insta','gmail']
-    },
-    {
-      name: 'ruchit',
-      email:'ruchit@test.com',
-      phone:'8888',
-      accounts: ['youtube','insta','gmail']
-    },
-    {
-      name: 'meet',
-      email:'meet@test.com',
-      phone:'8888',
-      accounts: ['linkedin','insta','gmail']
-    },
-
-  ]
+   title = 'style binding';
+  color = 'green';
+  bgColor = 'blue';
+  updateColor(){
+    this.color="gray";
+  }
 }
